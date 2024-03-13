d3ffbf697331e4bae8c09bc607d02020cc1ddd96
hackerover90 <isaac_noori@outlook.com>
2024-03-13 13:02:19 -0400

    Add functionality to increase score with each initial card click and update page layout and aesthetic

--- memory-card/src/App.jsx
+++ memory-card/src/App.jsx
@@ -7,6 +7,10 @@ function App() {
   const [score, setScore] = useState(0)
   const [highScore, setHighScore] = useState(0)
 
+  function increaseScore() {
+    setScore(score + 1)
+  }
+
   return (
     <>
       <div id='header'>
@@ -19,18 +23,30 @@ function App() {
       <div id='instructions'>Get points by clicking on an image but don't click on any more than once!</div>
       
       <div id='card-body'>
-        <Card id={'5cf5679a915ecad153ab68c9'} name={'Aang'}/>
-        <Card id={'5cf5679a915ecad153ab6992'} name={'Katara'}/>
-        <Card id={'5cf5679a915ecad153ab6a70'} name={'Toph'}/>
-        <Card id={'5cf5679a915ecad153ab68da'} name={'Azula'}/>
-        <Card id={'5cf5679a915ecad153ab6ab8'} name={'Zuko'}/>
-        <Card id={'5cf5679a915ecad153ab6a45'} name={'Sokka'}/>
-        <Card id={'5cf5679a915ecad153ab6a7a'} name={'Ty Lee'}/>
-        <Card id={'5cf5679a915ecad153ab6976'} name={'Iroh'}/>
-        <Card id={'5cf5679a915ecad153ab6a54'} name={'Suki'}/>
-        <Card id={'5cf5679a915ecad153ab68d4'} name={'Appa'}/>
-        <Card id={'5cf5679a915ecad153ab69fe'} name={'Ozai'}/>
-        <Card id={'5cf5679a915ecad153ab6a8a'} name={'Wan Shi Tong'}/>
+        <Card id={'5cf5679a915ecad153ab68c9'} name={'Aang'} 
+        increaseScore={increaseScore}/>
+        <Card id={'5cf5679a915ecad153ab6992'} name={'Katara'} 
+        increaseScore={increaseScore}/>
+        <Card id={'5cf5679a915ecad153ab6a70'} name={'Toph'} 
+        increaseScore={increaseScore}/>
+        <Card id={'5cf5679a915ecad153ab68da'} name={'Azula'} 
+        increaseScore={increaseScore}/>
+        <Card id={'5cf5679a915ecad153ab6ab8'} name={'Zuko'} 
+        increaseScore={increaseScore}/>
+        <Card id={'5cf5679a915ecad153ab6a45'} name={'Sokka'} 
+        increaseScore={increaseScore}/>
+        <Card id={'5cf5679a915ecad153ab6a7a'} name={'Ty Lee'} 
+        increaseScore={increaseScore}/>
+        <Card id={'5cf5679a915ecad153ab6976'} name={'Iroh'} 
+        increaseScore={increaseScore}/>
+        <Card id={'5cf5679a915ecad153ab6a54'} name={'Suki'} 
+        increaseScore={increaseScore}/>
+        <Card id={'5cf5679a915ecad153ab68d4'} name={'Appa'} 
+        increaseScore={increaseScore}/>
+        <Card id={'5cf5679a915ecad153ab69fe'} name={'Ozai'} 
+        increaseScore={increaseScore}/>
+        <Card id={'5cf5679a915ecad153ab6a8a'} name={'Wan Shi Tong'} 
+        increaseScore={increaseScore}/>
       </div>
     </>
   )
--- memory-card/src/Card.jsx
+++ memory-card/src/Card.jsx
@@ -1,31 +1,49 @@
 import { useState } from "react"
 import { useEffect } from "react"
 
-export default function Card({ id, name }) {
-    console.log('test1')
+export default function Card({ id, name, increaseScore }) {
     const [url, setUrl] = useState('')
+    const [clicked, setClicked] = useState(false)
     let link = `https://last-airbender-api.fly.dev/api/v1/characters/${id}`
-    let img_url = ''
-    fetch(link, {mode: 'cors'}) 
+    
+    useEffect(() => {
+        fetch(link, {mode: 'cors'}) 
         .then(response => {
             return response.json();
         })
         .then(response => {
-            console.log(response)
-            console.log('test2')
             setUrl(response.photoUrl)
         })
         .catch(error => {
             console.log('Error: ', error)
         })
+    }, [link])
     
+    /*
+    useEffect(() => {
+        if (clicked) {
+            alert('It is clicked')
+        }
+    }, [clicked])
+    */
+    
+    function handleCLick() {
+        if (!clicked) {
+            setClicked(true)
+        }
+        
+    }    
+
     
     return (
         
-        <div className="card" id={name}>
+        <div className="card" id={name} onClick={() => {
+            handleCLick();
+            if (!clicked) {
+                increaseScore()
+            }}}>
             <img src={url}/>
-            <div>{name}</div>
-            {console.log('test3')}
+            <div className="card-name">{name}</div>
         </div>
     )
 }
